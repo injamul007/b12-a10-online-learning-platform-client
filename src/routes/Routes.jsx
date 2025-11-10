981e4626f43c7b0a511b006e41bf4e44dfb78412
injamul007 <injamul007hoque@gmail.com>
2025-11-10 09:48:13 +0600

    setup routes with react router and add main layouts and choose primary and secondary color for website

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>
+  }
+])
+
+
+export default router;
