d22bc327f27525c075aff20b5772577c05ae4724
injamul007 <injamul007hoque@gmail.com>
2025-11-12 14:08:37 +0600

    create 2 axios custom hook name axiosInstance and axiosInstanceSecure for api calling

---
+++ src/hook/useAxiosSecure.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import React from 'react';
+import useAuth from './useAuth';
+
+
+const axiosInstanceSecure = axios.create({
+  baseURL: 'http://localhost:3000'
+})
+
+const useAxiosSecure = () => {
+  const {user} = useAuth();
+
+  //? set token in the header for all the api call using axiosSecure hook
+  axiosInstanceSecure.interceptors.request.use((config) => {
+    config.headers.authorization = `Bearer ${user.accessToken}`;
+    return config;
+  })
+
+  return axiosInstanceSecure;
+};
+
+export default useAxiosSecure;
