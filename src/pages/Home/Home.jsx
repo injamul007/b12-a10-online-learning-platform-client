2be73e55d10dc97646597e2aee590eae7c2a4704
injamul007 <injamul007hoque@gmail.com>
2025-11-13 21:24:57 +0600

    add why choose us static  sections for telling our website diagram

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import WhyChooseUs from '../../components/whyChoodeUs/WhyChooseUs';
 
 const Home = () => {
   return (
     <div>
       <title>SkilledHub || Home</title>
       <Banner></Banner>
+      <WhyChooseUs></WhyChooseUs>
     </div>
   );
 };
